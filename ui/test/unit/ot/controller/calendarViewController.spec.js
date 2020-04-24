0721631f2f8a537d256be887bb012bd9e1647899
venkatavineela <vineela.vini421.vv@gmail.com>
2020-04-24 13:38:58 +0530

    Siva, Vineela | MOBN-1161 | Update css for ot popups

--- ui/test/unit/ot/controller/calendarViewController.spec.js
+++ ui/test/unit/ot/controller/calendarViewController.spec.js
@@ -265,7 +265,7 @@ describe("calendarViewController", function () {
             template: "views/cancelAppointment.html",
             closeByDocument: false,
             controller: "calendarViewCancelAppointmentController",
-            className: 'ngdialog-theme-default ot-dialog',
+            className: 'ngdialog-theme-default ot-dialog popup',
             showClose: true,
             data: {
                 surgicalBlock: scope.surgicalBlockSelected,
@@ -284,7 +284,7 @@ describe("calendarViewController", function () {
             template: "views/cancelSurgicalBlock.html",
             closeByDocument: false,
             controller: "cancelSurgicalBlockController",
-            className: 'ngdialog-theme-default ot-dialog',
+            className: 'ngdialog-theme-default ot-dialog popup',
             showClose: true,
             data: {
                 surgicalBlock: scope.surgicalBlockSelected,
@@ -431,7 +431,7 @@ describe("calendarViewController", function () {
             template: "views/moveAppointment.html",
             closeByDocument: false,
             controller: "moveSurgicalAppointmentController",
-            className: 'ngdialog-theme-default ot-dialog',
+            className: 'ngdialog-theme-default ot-dialog popup',
             showClose: true,
             data: {
                 surgicalBlock: scope.surgicalBlockSelected,
@@ -485,7 +485,7 @@ describe("calendarViewController", function () {
         expect(ngDialog.open).toHaveBeenCalledWith(jasmine.objectContaining(
             {
                 template: 'views/surgicalAppointmentDialog.html',
-                className: 'ngdialog-theme-default',
+                className: 'ngdialog-theme-default popup',
                 closeByNavigation: true,
                 scope: scope,
                 data: surgicalAppointment
@@ -520,7 +520,7 @@ describe("calendarViewController", function () {
         expect(ngDialog.open).toHaveBeenCalledWith(jasmine.objectContaining(
             {
                 template: 'views/surgicalBlockDialog.html',
-                className: 'ngdialog-theme-default',
+                className: 'ngdialog-theme-default popup',
                 scope: scope,
                 data: surgicalBlock
             }
