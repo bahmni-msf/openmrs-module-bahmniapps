8157227e31f73673590aaea2114ac93a93033fb6
Vinisha Donthula <vinishad@thoughtworks.com>
2020-04-28 00:34:09 +0530

    Vinisha | MOBN-1211 | Add characterization tests for fetch function"

--- ui/test/unit/common/domain/services/observationsService.spec.js
+++ ui/test/unit/common/domain/services/observationsService.spec.js
@@ -81,4 +81,22 @@ describe("observationsService", function () {
        });
     });
 
+    describe("fetch", function () {
+        it("should send parameters specified in call to the server when obsIgnoreList, filterObsWithOrders and visitUuid are present", function () {
+            mockBackend.expectGET('/openmrs/ws/rest/v1/bahmnicore/observations?concept=conceptName1&concept=conceptName2&filterObsWithOrders=filterObsWithOrders&loadComplexData=false&obsIgnoreList=conceptName1&scope=latest&visitUuid=visitUuid').respond({results: ['Some data']});
+
+            observationsService.fetch(undefined, ["conceptName1", "conceptName2"], "latest", undefined, "visitUuid", ["conceptName1"], "filterObsWithOrders", undefined, false);
+
+            mockBackend.flush();
+        });
+
+        it("should send parameters specified in call to the server when obsIgnoreList, filterObsWithOrders and visitUuid are absent", function () {
+            mockBackend.expectGET('/openmrs/ws/rest/v1/bahmnicore/observations?concept=conceptName1&concept=conceptName2&loadComplexData=false&numberOfVisits=3&patientProgramUuid=patientprogramUuid&patientUuid=patientUuid&scope=latest').respond({results: ['Some data']});
+
+            observationsService.fetch("patientUuid", ["conceptName1", "conceptName2"], "latest", 3, undefined, undefined, null, "patientprogramUuid", false);
+
+            mockBackend.flush();
+        });
+    });
+
 });
