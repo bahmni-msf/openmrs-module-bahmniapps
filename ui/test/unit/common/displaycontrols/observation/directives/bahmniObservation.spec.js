9cbec13b0fd047865acc8e670202beb3b2baf783
vinishad <40575981+vinishad@users.noreply.github.com>
2020-04-29 13:43:08 +0530

    Vinisha | MOBN-1211 | Add new query search parameter numberOfEncounters and limit encounters for patient without programs in obs display control
    * Vinisha | MOBN-1211 | Add characterization tests for fetch function
    
    * Vinisha | MOBN-1211 | Add new query search parameter numberOfEncounters and limit encounters for patient without programs in obs display control

--- ui/test/unit/common/displaycontrols/observation/directives/bahmniObservation.spec.js
+++ ui/test/unit/common/displaycontrols/observation/directives/bahmniObservation.spec.js
@@ -113,7 +113,34 @@ describe("BahmniObservation", function () {
             expect(compiledElementScope).not.toBeUndefined();
             expect(compiledElementScope.config).not.toBeUndefined();
             expect(observationsService.fetch).toHaveBeenCalledWith(scope.patient.uuid, scope.config.conceptNames, scope.config.scope,
-                scope.config.numberOfVisits, undefined, undefined, null);
+                scope.config.numberOfVisits, undefined, undefined, null, null, undefined);
+            expect(observationsService.fetch.calls.count()).toEqual(1);
+            expect(observationsService.fetchForEncounter.calls.count()).toEqual(0);
+            expect(observationsService.fetchForPatientProgram.calls.count()).toEqual(0);
+        });
+
+        it("should fetch observations for patient if the numberofEncounters is provided", function () {
+            scope.patient = {uuid: '123'};
+            scope.config = {
+                showGroupDateTime: false,
+                conceptNames: ["Concept Name"],
+                scope: "latest",
+                numberOfVisits: 1,
+                numberOfEncounters: 2
+            };
+            scope.section = {};
+            observationsService.fetch.and.returnValue(specUtil.respondWithPromise(q, {data: {}}));
+
+            mockBackend.expectGET('../common/displaycontrols/observation/views/observationDisplayControl.html').respond("<div>dummy</div>");
+
+            var element = $compile(simpleHtml)(scope);
+            scope.$digest();
+            var compiledElementScope = element.isolateScope();
+            scope.$digest();
+
+            expect(compiledElementScope).not.toBeUndefined();
+            expect(compiledElementScope.config).not.toBeUndefined();
+            expect(observationsService.fetch).toHaveBeenCalledWith(scope.patient.uuid, scope.config.conceptNames, scope.config.scope, scope.config.numberOfVisits, undefined, undefined, null, null, 2);
             expect(observationsService.fetch.calls.count()).toEqual(1);
             expect(observationsService.fetchForEncounter.calls.count()).toEqual(0);
             expect(observationsService.fetchForPatientProgram.calls.count()).toEqual(0);
@@ -149,7 +176,7 @@ describe("BahmniObservation", function () {
             expect(compiledElementScope).not.toBeUndefined();
             expect(compiledElementScope.config).not.toBeUndefined();
             expect(observationsService.fetch).toHaveBeenCalledWith(scope.patient.uuid, scope.config.conceptNames,
-                scope.config.scope, scope.config.numberOfVisits, undefined, undefined, null);
+                scope.config.scope, scope.config.numberOfVisits, undefined, undefined, null, null, undefined);
             expect(observationsService.fetch.calls.count()).toEqual(1);
             expect(observationsService.fetchForEncounter.calls.count()).toEqual(0);
             expect(observationsService.fetchForPatientProgram.calls.count()).toEqual(0);
