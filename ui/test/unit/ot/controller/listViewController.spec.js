b32f2236ba19e115ed1fd23e289f2ec630cb9688
gopikrishna-yaramothu <gopikrishna.y@thoughtworks.com>
2021-03-29 16:32:51 +0530

    Gopi,Tarun | MOBN-1628 | Fixed Existing UI Tests

--- ui/test/unit/ot/controller/listViewController.spec.js
+++ ui/test/unit/ot/controller/listViewController.spec.js
@@ -368,7 +368,7 @@ describe('listViewController', function () {
 
 
     it("should sort the appointments by start date and by the location and by start time", function () {
-        scope.viewDate = moment('2017-02-22').toDate();
+        scope.viewDate = moment('2017-06-22').toDate();
         scope.filterParams = {
             providers: [],
             locations: {"OT 1": true, "OT 2": true, "OT 3": true},
@@ -376,7 +376,7 @@ describe('listViewController', function () {
             statusList: []
         };
         createController();
-        expect(surgicalAppointmentService.getSurgicalBlocksInDateRange).toHaveBeenCalledWith(jasmine.any(Date), jasmine.any(Date), true);
+        expect(surgicalAppointmentService.getSurgicalBlocksInDateRange).toHaveBeenCalledWith(jasmine.any(Date), jasmine.any(Date), true, true);
         expect(scope.surgicalAppointmentList.length).toEqual(6);
         expect(scope.surgicalAppointmentList[0].id).toEqual(104);
         expect(scope.surgicalAppointmentList[1].id).toEqual(105);
