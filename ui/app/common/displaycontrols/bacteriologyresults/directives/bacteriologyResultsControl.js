0f5eded77c1686f4ba55eea67c093b54056a8abe
Bindu <93639305+himabindu1238@users.noreply.github.com>
2023-10-18 17:34:07 +0530

    Bindu | MOBN-2219 | Remove Swab Results from Microbiology Display Control (#63)
    * Bindu | MOBN-2219 | Remove Swab Results from Microbiology Display Control
    
    * Bindu | MOBN-2219 | Refactored tests
    
    ---------
    
    Co-authored-by: Hima Bindu <himabindu1238@gmail.com>

--- ui/app/common/displaycontrols/bacteriologyresults/directives/bacteriologyResultsControl.js
+++ ui/app/common/displaycontrols/bacteriologyresults/directives/bacteriologyResultsControl.js
@@ -33,8 +33,10 @@ angular.module('bahmni.common.displaycontrol.bacteriologyresults')
                     return $scope.initializationPromise;
                 };
 
+
                 var handleResponse = function (response) {
                     $scope.observations = response.data.results;
+                    var excludedSpecimenSampleSources = $scope.section.excludedSpecimenSampleSources || [];
                     if ($scope.observations && $scope.observations.length > 0) {
                         $scope.specimens = [];
                         var sampleSource = _.find($scope.bacteriologyTabData.setMembers, function (member) {
@@ -49,10 +51,16 @@ angular.module('bahmni.common.displaycontrol.bacteriologyresults')
                         _.forEach($scope.observations, function (observation) {
                             $scope.specimens.push(specimenMapper.mapObservationToSpecimen(observation, $scope.allSamples, conceptsConfig, dontSortByObsDateTime));
                         });
+                        if(excludedSpecimenSampleSources.length > 0){
+                            const specimensWithoutExcludedSampleSources = _.filter($scope.specimens, function (specimen) {
+                            const sampleSourcesIncluded = excludedSpecimenSampleSources.find(sampleSource => sampleSource === specimen.specimenSource);
+                             return !sampleSourcesIncluded;
+                            });
+                            $scope.specimens = specimensWithoutExcludedSampleSources;
+                        }
                     } else {
                         $scope.specimens = [];
                     }
-
                     $scope.isDataPresent = function () {
                         if (!$scope.specimens || !$scope.specimens.length) {
                             return $scope.$emit("no-data-present-event") && false;
