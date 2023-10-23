132925f7f392c82a377d6974d4e19ec902055cc8
Hima Bindu <himabindu1238@gmail.com>
2023-10-23 15:07:49 +0530

    Bindu | MOBN-2219 | Refactored bacteriologyResultsControl

--- ui/app/common/displaycontrols/bacteriologyresults/directives/bacteriologyResultsControl.js
+++ ui/app/common/displaycontrols/bacteriologyresults/directives/bacteriologyResultsControl.js
@@ -52,8 +52,8 @@ angular.module('bahmni.common.displaycontrol.bacteriologyresults')
                         });
                         if (excludedSpecimenSampleSources.length > 0) {
                             const specimensWithoutExcludedSampleSources = _.filter($scope.specimens, function (specimen) {
-                                const sampleSourcesIncluded = excludedSpecimenSampleSources.find(function (sampleSource) {
-                                    sampleSource === specimen.specimenSource;
+                                const sampleSourcesIncluded = _.find(excludedSpecimenSampleSources, function (sampleSource) {
+                                    return sampleSource === specimen.specimenSource;
                                 });
                                 return !sampleSourcesIncluded;
                             });
