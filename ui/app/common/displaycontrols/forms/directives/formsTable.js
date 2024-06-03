ed9161d8137794c9290b670a654284b9371ca7d9
keerthi.magantii <keerthisai.maganti@thoughtworks.com>
2024-06-03 12:52:36 +0530

    Keerthi rename selectShortNameForForms config key value

--- ui/app/common/displaycontrols/forms/directives/formsTable.js
+++ ui/app/common/displaycontrols/forms/directives/formsTable.js
@@ -6,7 +6,7 @@ angular.module('bahmni.common.displaycontrol.forms')
             var defaultController = function ($scope) {
                 $scope.shouldPromptBrowserReload = true;
                 $scope.showFormsDate = appService.getAppDescriptor().getConfigValue("showFormsDate");
-                $scope.selectShortName = appService.getAppDescriptor().getConfigValue("selectShortName");
+                $scope.selectShortNameForForms = appService.getAppDescriptor().getConfigValue("selectShortName");
                 var getAllObservationTemplates = function () {
                     return conceptSetService.getConcept({
                         name: "All Observation Templates",
@@ -81,7 +81,7 @@ angular.module('bahmni.common.displaycontrol.forms')
                     var concept = data.concept;
                     var defaultLocale = $rootScope.currentUser.userProperties.defaultLocale;
                     var displayName;
-                    if($scope.selectShortName ==true){
+                    if($scope.selectShortName == true){
                         displayName = getLocaleSpecificConceptName(concept, defaultLocale, "SHORT");
                     }
                     else {
