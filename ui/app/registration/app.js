11b77e97048ca034e0ecf158c201746f1d139a26
saisamyukthag <saisamyuktha.g@thoughtworks.com>
2024-06-09 16:27:46 +0530

    Samyuktha | Fix. Title of photo capture popup.

--- ui/app/registration/app.js
+++ ui/app/registration/app.js
@@ -86,6 +86,7 @@ angular
     }]).run(['$rootScope', '$templateCache', '$bahmniCookieStore', 'locationService', 'messagingService', 'auditLogService',
         '$window', function ($rootScope, $templateCache, $bahmniCookieStore, locationService,
               messagingService, auditLogService, $window) {
+            moment.locale($window.localStorage["NG_TRANSLATE_LANG_KEY"] || "en");
             var getStates = function (toState, fromState) {
                 var states = [];
                 if (fromState === "newpatient" && (toState === "patient.edit" || toState === "patient.visit")) {
@@ -98,7 +99,6 @@ angular
                 }
                 return states;
             };
-            moment.locale($window.localStorage["NG_TRANSLATE_LANG_KEY"] || "en");
             var loginLocationUuid = $bahmniCookieStore.get(Bahmni.Common.Constants.locationCookieName).uuid;
             locationService.getVisitLocation(loginLocationUuid).then(function (response) {
                 if (response.data) {
