034076a2c46b8c05e86b7de76d918a85a7215831
rsrilaxmiragini <srilaxmiragini.r@thoughtworks.com>
2024-11-06 15:34:10 +0530

    SreeLekha, Sri Laxmi | MOBN-2522 | Update. Address fields in visit tab patient ribbon.

--- ui/app/clinical/common/controllers/visitController.js
+++ ui/app/clinical/common/controllers/visitController.js
@@ -118,6 +118,8 @@ angular.module('bahmni.clinical')
 
             var init = function () {
                 $scope.hideDoctorName = appService.getAppDescriptor().getConfigValue("hideDoctorName");
+                $scope.hidePOAddress = appService.getAppDescriptor().getConfigValue("hidePOAddress");
+                $scope.hideVillageAddress = appService.getAppDescriptor().getConfigValue("hideVillageAddress");
                 $scope.visitTabConfig.setVisitUuidsAndPatientUuidToTheSections([$scope.visitUuid], $scope.patientUuid);
                 var tabToOpen = getTab();
                 $scope.visitTabConfig.switchTab(tabToOpen);
